0b419e2ddd4ccf1e235a4ee6a7a89072600e413e
zainul.abidin <zainul.abidin@unmer.ac.id>
2026-08-04 15:32:04 +0700

    fix(core): add AuthorizesRequests trait to base controller

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Traits\ApiResponseTrait;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller
 {
-    use ApiResponseTrait;
+    use ApiResponseTrait, AuthorizesRequests;
 }
